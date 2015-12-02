31636cc95ccc69c81ac64f4b968fcee587ae7851
Ten-Young Guh <tenyoung795@gmail.com>
2015-12-02 02:38:50 -0500

    Further revise common.h; remove struct
    The struct is probably unnecessary

--- common.h
+++ common.h
@@ -10,34 +10,22 @@ typedef enum {
     CS428_METADATA = 0,
     CS428_CONTENT = 1,
 } cs428_packet_type_t;
+
 enum {
     CS428_FILENAME_MAX = 256,
-    CS428_CONTENT_SIZE = 512,
-};
-
-typedef struct {
-    char filename[CS428_FILENAME_MAX];
-    char filesize[sizeof(cs428_filesize_t)];
-} cs428_metadata_t;
-
-typedef struct {
-    char sequence_number[sizeof(cs428_seq_no_t)];
-    char packet_type;
-    union {
-        cs428_metadata_t metadata;
-        char content[CS428_CONTENT_SIZE];
-    };
-} cs428_packet_t;
-
-enum {
+    CS428_MAX_CONTENT_SIZE = 512,
     CS428_MIN_PACKET_SIZE =
         sizeof(cs428_seq_no_t) // sequence number
+        + 1, // packet type
+    CS428_MAX_PACKET_SIZE =
+        sizeof(cs428_seq_no_t) // sequence number
         + 1 // packet type
-        + 1, // content
+        + CS428_MAX_CONTENT_SIZE, // content
     CS428_START_PACKET_SIZE = 
         sizeof(cs428_seq_no_t) // sequence number
         + 1 // packet type
-        + sizeof(cs428_metadata_t),
+        + CS428_FILENAME_MAX // filename
+        + sizeof(cs428_filesize_t), // filesize
     CS428_WINDOW_SIZE = 16,
     CS428_SERVER_PORT = 9999,
 };
