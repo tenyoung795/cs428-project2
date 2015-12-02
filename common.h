e94fbf2dbc9dd9cebbfd47132590c122f18bf9fc
Ten-Young Guh <tenyoung795@gmail.com>
2015-12-02 01:41:44 -0500

    Add packet size and window size constants

--- common.h
+++ common.h
@@ -24,6 +24,11 @@ typedef struct {
 } cs428_packet_t ;
 
 enum {
+    CS428_PACKET_SIZE =
+        8 // sequence number
+        + 1 // packet type
+        + 512, // content
+    CS428_WINDOW_SIZE = 16,
     CS428_SERVER_PORT = 9999,
 };
 
