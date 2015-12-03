ac0a280c7819c91a248c4d7b35faace13a592c85
Ten-Young Guh <tenyoung795@gmail.com>
2015-12-03 13:51:50 -0500

    Remove verbose uint64_t typedefs

--- common.h
+++ common.h
@@ -4,8 +4,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef uint64_t cs428_seq_no_t;
-typedef uint64_t cs428_filesize_t;
 typedef enum {
     CS428_METADATA = 0,
     CS428_CONTENT = 1,
@@ -15,17 +13,17 @@ enum {
     CS428_FILENAME_MAX = 256,
     CS428_MAX_CONTENT_SIZE = 512,
     CS428_MIN_PACKET_SIZE =
-        sizeof(cs428_seq_no_t) // sequence number
+        8 // sequence number
         + 1, // packet type
     CS428_MAX_PACKET_SIZE =
-        sizeof(cs428_seq_no_t) // sequence number
+        8 // sequence number
         + 1 // packet type
         + CS428_MAX_CONTENT_SIZE, // content
     CS428_START_PACKET_SIZE = 
-        sizeof(cs428_seq_no_t) // sequence number
+        8 // sequence number
         + 1 // packet type
         + CS428_FILENAME_MAX // filename
-        + sizeof(cs428_filesize_t), // filesize
+        + 8, // filesize
     CS428_WINDOW_SIZE = 16,
     CS428_SERVER_PORT = 9999,
 };
